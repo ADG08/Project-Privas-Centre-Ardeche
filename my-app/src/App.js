4078443bfceffd4fc4427dcc6d606b958f9ea729
ADG08 <49250104+ADG08@users.noreply.github.com>
2022-10-04 16:19:10 +0200

    Ready

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,9 @@
-import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Layout from "./Pages/Layout";
 import Environnement from "./Pages/Environnement";
+import Culture from "./Pages/Culture";
+
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/Environnement" element={<Environnement />} />
+          <Route path="/Culture" element={<Culture />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ my-app/src/Pages/Culture.js
@@ -0,0 +1,11 @@
+import "./Culture.css"
+
+function Culture() {
+  return (
+    <div>
+      <h1>TU ECRIS LA</h1>
+    </div>
+  );
+}
+
+export default Culture;
