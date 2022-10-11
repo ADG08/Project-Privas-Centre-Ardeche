9d7b6c8dc725df58e1c5b5b307d16de20d496962
Lisoma23 <sf.hazami@gmail.com>
2022-10-11 16:05:42 +0200

    Titre et image de fond

--- my-app/src/Pages/Environnement.js
+++ my-app/src/Pages/Environnement.js
@@ -1,11 +1,12 @@
 import "./Environnement.css"
-
+import BackgroundImageTopEnv from "../Images/Environnement/env_home.png"
 function Environnement() {
   return (
     <div>
-      <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARG</h1>
+      <h1 className="env">Environnement</h1>
+      <img className="imgenv" src={BackgroundImageTopEnv} alt=""></img>
     </div>
+   
   );
 }
-
 export default Environnement;
