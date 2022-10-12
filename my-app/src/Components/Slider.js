dfc314324964fa58dce8790f6ff3bd118a31311f
ADG08 <49250104+ADG08@users.noreply.github.com>
2022-10-12 13:59:27 +0200

    aehfuzgefouez

--- my-app/src/Components/Slider.js
+++ my-app/src/Components/Slider.js
@@ -3,6 +3,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 import "./Slider.css"
 import image from "../Images/thum_privas-6.jpg"
+import { Link } from "react-router-dom";
 
 function Slider() {
   const datas = [
@@ -16,7 +17,7 @@ function Slider() {
       id: 2,
       image: `${image}`,
       title: "test2",
-      text: "blablablaebadqve",
+      text: "bonjour je suis la salut c'est moi coucou heho",
     }, {
       id: 3,
       image: `${image}`,
@@ -33,6 +34,7 @@ function Slider() {
           <div className="overlay">
             <h2 className="overlay_title">{slide.title}</h2>
             <p className="overlay_text">{slide.text}</p>
+            <Link to="/Travaux">Lire la suite</Link>
           </div>
         </div>
       ))}
