e7d561e1bd24cf7a276dbbec14b57cc27ce57043
babyIsk <115020880+babyIsk@users.noreply.github.com>
2022-10-12 02:16:35 +0200

    premiere et deuxième partie (pas finish)

--- my-app/src/Pages/Culture.js
+++ my-app/src/Pages/Culture.js
@@ -1,14 +1,26 @@
 import "./Culture.css"
+import imageFond from "../Images/culture/biblio_page_culture.jpg"
+import encadreTitre from "../Images/culture/encadre.png"
+import imgPresentation from "../Images/culture/img_presentation.png"
 
 function Culture() {
   return (
-    <div>
-      <h1>TU ECRIS LA</h1>
-      <p>hehe</p>
+    <div className="body">
+
+      <div className="premierePart">
+        <img src = {imageFond} alt = "" className="imgFond"></img>
+        <img src = {encadreTitre} alt = "" className="encadreTitre"></img>
+        <h1 className="titre">Culture</h1>
+      </div>
+
+      <div className ="deuxiemePart">
+        <div className = "lignes"></div>
+        <img src= {imgPresentation} alt="" className="imgPresentation"></img>
+        <div className = "lignes"></div>
+
+      </div>
     </div>
-    
   );
-  
 }
 
 export default Culture;
