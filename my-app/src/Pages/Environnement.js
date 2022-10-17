0067ede798a83e470d5e21a91532b9ff822baaaa
ADG08 <dominguesramosandre@gmail.com>
2022-10-17 14:46:51 +0200

    j

--- my-app/src/Pages/Environnement.js
+++ my-app/src/Pages/Environnement.js
@@ -5,12 +5,10 @@ import Titre_env from "../Images/Titres.png"
 function Environnement() {
   return (
     <div>
-      
       <img className="imgtitrenv" src={Titre_env} alt=""></img>
       <img className="imgenv" src={BackgroundImageTopEnv} alt=""></img>
       <h1 className="env">Environnement</h1>
     </div>
-   
   );
 }
 export default Environnement;
