5c51f52995586d64095eb98c0e368b65b57d8fde
ADG08 <dominguesramosandre@gmail.com>
2022-10-24 14:58:20 +0200

    yes

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,6 +1,14 @@
-import RapidButton from "../Components/RapidButton"
-import test from "../Images/Home/IMG_4495.jpg"
 import "./Home.css"
+
+import presentationImg from "../Images/Home/presentation.png"
+import fonctionnementImg from "../Images/Home/fonctionnement.png"
+import competenceImg from "../Images/Home/competences.png"
+import communeImg from "../Images/Home/communes.png"
+import territoireImg from "../Images/Home/territoire.png"
+import contactImg from "../Images/Home/contact.png"
+import poubellesImg from "../Images/Home/poubelles.jpg"
+
+import RapidButton from "../Components/RapidButton"
 import Slider from "../Components/Slider.js"
 import Typewriter from "typewriter-effect"
 
@@ -12,25 +20,28 @@ function Home() {
           onInit={(typewriter) =>{
             typewriter
               .pauseFor(1000)
-              .typeString("Envie de montagne ?")
+              .typeString("Envie de s'évader ?")
+              .pauseFor(1000)
+              .deleteChars(10)
+              .typeString("montagne ?")
               .pauseFor(1000)
               .deleteChars(12)
               .typeString("'histoire ?")
               .pauseFor(1000)
               .deleteAll()
-              .typeString("BIENVENUE A PRIVAS CENTRE ARDECHE")
+              .typeString("Bienvenue à   Privas Centre Ardèche")
               .start()
           }}
         />
       </div>
       <h1 className="ButtonTitle">Notre Communauté</h1>
       <div className="ButtonDiv">
-        <RapidButton image={test} text="Présentation" to="/Travaux" />
-        <RapidButton image={test} text="Fonctionnement" to="/Travaux" />
-        <RapidButton image={test} text="Compétences" to="/Travaux" />
-        <RapidButton image={test} text="Communes" to="/Travaux" />
-        <RapidButton image={test} text="Territoire" to="/Travaux" />
-        <RapidButton image={test} text="Contact" to="/Travaux" />
+        <RapidButton image={presentationImg} text="Présentation" to="/Travaux" />
+        <RapidButton image={fonctionnementImg} text="Fonctionnement" to="/Travaux" />
+        <RapidButton image={competenceImg} text="Compétences" to="/Travaux" />
+        <RapidButton image={communeImg} text="Communes" to="/Travaux" />
+        <RapidButton image={territoireImg} text="Territoire" to="/Travaux" />
+        <RapidButton image={contactImg} text="Contact" to="/Travaux" />
       </div>
       <div className="firstCarousel">
         <h1>Les Actus</h1>
