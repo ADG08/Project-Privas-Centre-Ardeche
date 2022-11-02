7f65134bffc7b6f70fd8675a85953a828ce19bbd
Lisoma23 <sf.hazami@gmail.com>
2022-11-02 19:33:09 +0100

    terminado

--- my-app/src/Pages/DecouvrirPrivas.js
+++ my-app/src/Pages/DecouvrirPrivas.js
@@ -9,7 +9,6 @@ import office from "../Images/Decouvrir-Privas/agence_tourrisme.png"
 import vernoux from "../Images/Decouvrir-Privas/vernoux.png"
 import rando from "../Images/Decouvrir-Privas/randonnées.png"
 import passe from "../Images/Decouvrir-Privas/dupasse.png"
-import "./DecouvrirPrivas.js"
 
 function Decouvrir_Privas() {
   return (
@@ -19,7 +18,7 @@ function Decouvrir_Privas() {
         <div className="titredec">
           <h1 className="dec">Découvrir Privas</h1>
         </div>
-        <div className="ButtonDivDec">
+        <div className="ButtonDivdec">
           <RapidButton image={tourisme_img} text="Tourrisme" to="/Travaux" />
           <RapidButton image={Communes_img} text="Communes" to="/Travaux" />
           <RapidButton image={territoire_img} text="Un territoire à forte identité" to="/Travaux" />
@@ -29,10 +28,10 @@ function Decouvrir_Privas() {
       <div className="deuxiemePartdec">
         <div className="lignesV1dec"></div>
         <div className="bannieredec">
-          <div className="paragraphedec"><p>La Communauté d’Agglomération Privas Centre Ardèche est engagée, 
-            avec ses partenaires, dans le développement des activités liées au tourisme. A proximité de la 
-            caverne ornée du Pont d’Arc, inscrite au patrimoine mondial de l’UNESCO, des milliers de visiteurs 
-            découvrent chaque année les 42 villes et villages de la Communauté d’Agglomération 
+          <div className="paragraphedec"><p>La Communauté d’Agglomération Privas Centre Ardèche est engagée,
+            avec ses partenaires, dans le développement des activités liées au tourisme. A proximité de la
+            caverne ornée du Pont d’Arc, inscrite au patrimoine mondial de l’UNESCO, des milliers de visiteurs
+            découvrent chaque année les 42 villes et villages de la Communauté d’Agglomération
             Privas Centre Ardèche.</p>
           </div>
           <img src={imgPresentationdec} alt="" className="imgPresentationdec"></img>
@@ -43,11 +42,11 @@ function Decouvrir_Privas() {
       <div className="pausePartdec"></div>
 
       <div className="troisiemePartdec">
-          <h1 className="ButtonTitledec">Liens rapides</h1>
+        <h1 className="ButtonTitledec">Liens rapides</h1>
         <div className="ButtonDivdec">
-          <RapidButton image={office} text="Office de tourrisme" to="/Travaux" /> 
+          <RapidButton image={office} text="Office de tourrisme" to="/Travaux" />
           <RapidButton image={vernoux} text="Plateaux du Vernoux" to="/Travaux" />
-          <RapidButton image={rando} text="Chemins de randonnées" to="/Travaux" />
+          <RapidButton image={rando} text="Pour vos randonnées" to="/Travaux" />
           <RapidButton image={passe} text="Du passé au présent" to="/Travaux" />
         </div>
       </div>
